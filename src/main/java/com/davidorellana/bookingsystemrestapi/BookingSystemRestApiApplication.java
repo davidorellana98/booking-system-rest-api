0356dee2d3ad8ba9bd05e5939795bc33574bd20d
David Orellana <davidorellana_98@hotmail.com>
2022-11-02 20:17:01 -0500

    Initial commit

---
+++ src/main/java/com/davidorellana/bookingsystemrestapi/BookingSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.davidorellana.bookingsystemrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidorellana/bookingsystemrestapi/BookingSystemRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidorellana.bookingsystemrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
