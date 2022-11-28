97d390e01c7d35c6e555d447939c58a06e82cd73
David Orellana <davidorellana_98@hotmail.com>
2022-11-27 23:47:40 -0500

    Service layer modification, for implementation with repository interfaces.

--- src/main/java/com/davidorellana/bookingsystemrestapi/user/service/UserService.java
+++ src/main/java/com/davidorellana/bookingsystemrestapi/user/service/UserService.java
@@ -1,14 +1,15 @@
 package com.davidorellana.bookingsystemrestapi.user.service;
 
 import com.davidorellana.bookingsystemrestapi.user.model.data.User;
+import com.davidorellana.bookingsystemrestapi.user.model.dto.UserDto;
 
 import java.util.HashMap;
 
-public interface UserServiceI {
+public interface UserService {
 
-    HashMap<Long, User> allUsers();
+    HashMap<Long, User> getAllUsers();
     User findUserById(Long idUser);
-    User createUser(User user);
-    User updateUserById(Long idUser, User user);
-    User deleteUserById(Long idUser);
+    User createUser(UserDto userDto);
+    User updateUserById(Long idUser, UserDto userDto);
+    Boolean deleteUserById(Long idUser);
 }
