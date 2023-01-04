877bafd6acbe1d8122c493fd5e4477a8cc91310f
David Orellana <davidorellana_98@hotmail.com>
2023-01-03 22:48:58 -0500

    Implementation of @EnableMongoRepositories annotation in Spring main.

--- src/main/java/com/davidorellana/bookingsystemrestapi/BookingSystemRestApiApplication.java
+++ src/main/java/com/davidorellana/bookingsystemrestapi/BookingSystemRestApiApplication.java
@@ -2,8 +2,10 @@ package com.davidorellana.bookingsystemrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class BookingSystemRestApiApplication {
 
 	public static void main(String[] args) {
