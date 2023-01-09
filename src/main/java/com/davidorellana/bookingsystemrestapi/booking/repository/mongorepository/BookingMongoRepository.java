47b9c8ee2b651df06041e221b561e8e28bc8f219
David Orellana <davidorellana_98@hotmail.com>
2023-01-09 09:10:04 -0500

    Small modifications to the images and removal of log properties.

--- src/main/java/com/davidorellana/bookingsystemrestapi/booking/repository/mongorepository/BookingMongoRepository.java
+++ src/main/java/com/davidorellana/bookingsystemrestapi/booking/repository/mongorepository/BookingMongoRepository.java
@@ -2,15 +2,12 @@ package com.davidorellana.bookingsystemrestapi.booking.repository.mongorepositor
 
 import com.davidorellana.bookingsystemrestapi.booking.model.data.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface BookingMongoRepository extends MongoRepository<Booking, String> {
 
-    //@Query(value = "{'bookingType' : ?0}", exists = true)
     List<Booking> findBookingsByBookingType(String bookingType);
 
-    //@Query(value = "{'paymentMethods' : ?0}", exists = true)
     List<Booking> findBookingsByPaymentMethods(String paymentMethods);
 }
